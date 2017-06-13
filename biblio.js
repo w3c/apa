973ee50a4fc01231cd25d2612341421986885559
michael-n-cooper <cooper@w3.org>
2017-06-13 12:52:29 -0400

    Set up revision of CAPTCHA doc

---
+++ biblio.js
@@ -0,0 +1,124 @@
+var biblio = {
+	"AICAPTCHA": {
+		"title": "aiCaptcha: Using AI to beat CAPTCHA and post comment spam",
+		"publishDate": "",
+		"authors": ["Casey Chesnut"],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "",
+		"href": "http://www.brains-n-brawn.com/default.aspx?vDir=aicaptcha"
+	},
+	"ANTIPHISHING": {
+		"title": "Phishing Activity Trends Report",
+		"publishDate": "July, 2005",
+		"authors": [],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "Anti-Phishing Working Group",
+		"href": "http://antiphishing.org/APWG_Phishing_Activity_Report_Jul_05.pdf"
+	},
+	"BOOKSHARE": {
+		"title": "Bookshare.org home page",
+		"publishDate": "",
+		"authors": [],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "",
+		"href": "http://www.bookshare.org"
+	},
+	"BREAKING": {
+		"title": "Breaking CAPTCHAs Without Using OCR",
+		"publishDate": "",
+		"authors": ["Howard Yeend"],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "",
+		"href": "http://www.cs.berkeley.edu/%7Emori/gimpy/gimpy.html"
+	},
+	"BREAKINGOCR": {
+		"title": "Breaking CAPTCHAs Without Using OCR",
+		"publishDate": "",
+		"authors": ["Howard Yeend"],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "",
+		"href": "http://www.puremango.co.uk/cm_breaking_captcha_115.php"
+	},
+	"CAPTCHA": {
+		"title": "The CAPTCHA Project",
+		"publishDate": "",
+		"authors": [],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "Carnegie Mellon University",
+		"href": "http://www.captcha.net"
+	},
+	"CHAFEE": {
+		"title": "17 USC 121, Limitations on exclusive rights: reproduction for blind or other people with disabilities (also known as the Chafee Amendment)",
+		"publishDate": "",
+		"authors": [],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "",
+		"href": "http://www.copyright.gov/title17/92chap1.html"
+	},
+	"KILLBOTS": {
+		"title": "Botz-4-Sale: Surviving DDos Attacks that Mimic Flash Crowds",
+		"publishDate": "",
+		"authors": [
+			"Srikanth Kandula", 
+			"Dina Katabi", 
+			"Matthias Jacob", 
+			"Arthur Burger"
+		],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "",
+		"href": "http://www.usenix.org/events/nsdi05/tech/kandula/kandula_html/"
+	},
+	"NEWSCOM": {
+		"title": "Spam-bot tests flunk the blind",
+		"publishDate": "2 July 2003",
+		"authors": ["Paul Festa"],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "News.com",
+		"href": "http://news.com.com/2100-1032-1022814.html"
+	},
+	"PINGUARD": {
+		"title": "PIN Guard",
+		"publishDate": "",
+		"authors": [],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "ING Direct site",
+		"href": "https://secure1.ingdirect.com/tpw/popup_whatIsThis.html"
+	},
+	"PWNTCHA": {
+		"title": "PWNtcha - CAPTCHA decoder",
+		"publishDate": "",
+		"authors": ["Sam Hocevar"],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "",
+		"href": "http://sam.zoy.org/pwntcha/"
+	},
+	"TRACKBACK": {
+		"title": "Trackback",
+		"publishDate": "",
+		"authors": [],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "Wikipedia",
+		"href": "http://en.wikipedia.org/wiki/Trackback"
+	},
+	"TURING": {
+		"title": "The Turing Test",
+		"publishDate": "2002",
+		"authors": [],
+		"editors": [],
+		"etAl": "false",
+		"publisher": "he Alan Turing Internet Scrapbook",
+		"href": "http://www.turing.org.uk/turing/scrapbook/test.html"
+	}
+}
---
+++ captcha/respec-config.js
@@ -0,0 +1,94 @@
+var respecConfig = {
+	// embed RDFa data in the output
+	trace:  true,
+	useExperimentalStyles: true,
+	doRDFa: '1.1',
+	includePermalinks: true,
+	permalinkEdge:     true,
+	permalinkHide:     false,
+	tocIntroductory: true,
+	// specification status (e.g., WD, LC, NOTE, etc.). If in doubt use ED.
+	specStatus:           "ED",
+	//crEnd:                "",
+	//perEnd:               "",
+	diffTool:             "http://www.aptest.com/standards/htmldiff/htmldiff.pl",
+	
+	// the specifications short name, as in https://www.w3.org/TR/short-name/
+	shortName:            "turingtest-2",
+	
+	
+	// if you wish the publication date to be other than today, set this
+	//publishDate:  "2014-12-11",
+	copyrightStart:  "2017",
+	
+	// if there is a previously published draft, uncomment this and set its YYYY-MM-DD date
+	// and its maturity status
+	//previousPublishDate:  "",
+	//previousMaturity:  "WD",
+	//prevRecURI: "",
+	//previousDiffURI: "",
+	
+	// if there a publicly available Editors Draft, this is the link
+	edDraftURI: "https://w3c.github.io/apa/captcha/",
+	
+	// if this is a LCWD, uncomment and set the end of its review period
+	// lcEnd: "2012-02-21",
+	
+	// editors, add as many as you like
+	// only "name" is required
+	editors: [
+		{
+			name: "Janina Sajka",
+			url: "http://rednote.net/",
+			mailto: "janina@rednote.net",
+			w3cid: 33688
+		},
+		{
+			name: "Michael Cooper",
+			url: 'https://www.w3.org',
+			mailto: "cooper@w3.org",
+			company: "W3C",
+			companyURI: "https://www.w3.org",
+			w3cid: 34017
+		}
+	],
+	
+	// authors, add as many as you like.
+	// This is optional, uncomment if you have authors as well as editors.
+	// only "name" is required. Same format as editors.
+	
+	//authors:  [
+	//    { name: "Your Name", url: "http://example.org/",
+	//      company: "Your Company", companyURI: "http://example.com/" },
+	//],
+	
+	/*
+	alternateFormats: [
+		{ uri: 'wcag21-diff.html', label: "Diff from Previous Recommendation" } ,
+		{ uri: 'wcag21.ps', label: "PostScript version" },
+		{ uri: 'wcag21.pdf', label: "PDF version" }
+	],
+	*/
+	
+	// errata: 'https://www.w3.org/2010/02/rdfa/errata.html',
+	
+	// name of the WG
+	wg:           "Accessible Platform Architectures Working Group",
+	
+	// URI of the public WG page
+	wgURI:        "https://www.w3.org/WAI/APA/",
+	
+	// name (with the @w3c.org) of the public mailing to which comments are due
+	wgPublicList: "public-apa@w3.org",
+	
+	// URI of the patent status for this WG, for Rec-track documents
+	// !!!! IMPORTANT !!!!
+	// This is important for Rec-track documents, do not copy a patent URI from a random
+	// document unless you know what you're doing. If in doubt ask your friendly neighbourhood
+	// Team Contact.
+	wgPatentURI:  "https://www.w3.org/2004/01/pp-impl/83907/status",
+	maxTocLevel: 4,
+	
+	localBiblio: biblio
+	
+};
