c497212a5f5b2e76fc2a59ff205dc1cc2ee5d71a
michael-n-cooper <cooper@w3.org>
2018-05-15 16:52:32 -0400

    convert captcha references

--- biblio.js
+++ biblio.js
@@ -120,5 +120,185 @@ var biblio = {
 		"etAl": "false",
 		"publisher": "he Alan Turing Internet Scrapbook",
 		"href": "http://www.turing.org.uk/turing/scrapbook/test.html"
-	}
+	},
+    "eval-audio": {
+        "authors": [
+            "Bigham, J. P.",
+            "Cavender, A. C."
+        ],
+        "date": "April 2009",
+        "title": "Evaluating existing audio CAPTCHAs and an interface optimized for non-visual use",
+        "publisher": "In Proceedings of the SIGCHI Conference on Human Factors in Computing Systems"
+    },
+    "video-events": {
+        "authors": [
+            "Catuogno, L.",
+            "Galdi, C."
+        ],
+        "date": "2014",
+        "title": "On user authentication by means of video events recognition",
+        "publisher": "Journal of Ambient Intelligence and Humanized Computing 5(6)",
+        "pages": "909-918",
+        "doi": "doi:10.1007/s12652-014-0248-5"
+    },
+    "auth-mult": {
+        "authors": ["Cetin, C."],
+        "date": "2015",
+        "title": "Design, Testing and Implementation of a New Authentication Method Using Multiple Devices",
+        "publisher": "J. Ligatti, D. Goldgof, & Y. Liu (Eds.): ProQuest Dissertations Publishing"
+    },
+    "captchastar": {
+        "authors": [
+            "Conti, M.",
+            "Guarisco, C.",
+            "Spolaor, R."
+        ],
+        "date": "2015",
+        "title": "CAPTCHaStar! A novel CAPTCHA based on interactive shape discovery"
+    },
+    "captcha-ld": {
+        "authors": [
+            "Gafni, R.",
+            "Nagar, I."
+        ],
+        "title": "The Effect of CAPTCHA on User Experience among Users with and without Learning Disabilities"
+    },
+    "civil-rights-captcha": {
+        "authors": [
+            "Hernández-Castro, C. J.",
+            "Barrero, D. F.",
+            "R-Moreno, M. D."
+        ],
+        "date": "2016",
+        "title": "Machine learning and empathy: the Civil Rights CAPTCHA",
+        "publisher": "Concurrency and Computation: Practice and Experience, 28(4)",
+        "pages": "1310-1323",
+        "doi": "doi:10.1002/cpe.3632"
+    },
+    "facecaptcha": {
+        "authors": [
+            "Kim, J.",
+            "Kim, S.",
+            "Yang, J.",
+            "Ryu, J.-h.",
+            "Wohn, K."
+        ],
+        "date": "2014",
+        "title": "FaceCAPTCHA: a CAPTCHA that identifies the gender of face images unrecognized by existing gender classifiers",
+        "publisher": "An International Journal, 72(2)",
+        "pages": "1215-1237. ",
+        "doi": "doi:10.1007/s11042-013-1422-z"
+    },
+    "video-captcha-security": {
+        "authors": ["Kluever, K."],
+        "date": "2008",
+        "title": "Evaluating the usability and security of a video CAPTCHA",
+        "publisher": "R. Zanibbi, Z. Butler, & R. Canosa (Eds.): ProQuest Dissertations Publishing."
+    },
+    "social-classification": {
+        "authors": ["Korayem, M."],
+        "date": "2015",
+        "title": "Social and egocentric image classification for scientific and privacy applications",
+        "publisher": "D. Crandall, J. Bollen, A. Kapadia, & P. Radivojac (Eds.): ProQuest Dissertations Publishing."
+    },
+    "facial-captcha-attack": {
+        "authors": ["Li, Q."],
+        "date": "2015",
+        "title": "A computer vision attack on the ARTiFACIAL CAPTCHA",
+        "publisher": "An International Journal, 74(13)",
+        "pages": "4583-4597",
+        "doi": "doi:10.1007/s11042-013-1823-z"
+    },
+    "defeat-line-noise": {
+        "authors": [
+            "Nakaguro, Y.",
+            "Dailey, M. N.",
+            "Marukatat, S.",
+            "Makhanov, S. S."
+        ],
+        "date": "2013",
+        "title": "Defeating line-noise CAPTCHAs with multiple quadratic snakes",
+        "publisher": "Computers & Security, 37",
+        "pages": "91-110",
+        "doi": "doi:10.1016/j.cose.2013.05.003"
+    },
+    "3d-captcha-security": {
+        "authors": [
+            "Nguyen, V. D.",
+            "Chow, Y.-W.",
+            "Susilo, W."
+        ],
+        "date": "2014",
+        "title": "On the security of text-based 3D CAPTCHAs",
+        "publisher": "Computers & Security, 45",
+        "pages": "84-99",
+        "doi": "doi:10.1016/j.cose.2014.05.004"
+    },
+    "recaptcha-attacks": {
+        "authors": [
+            "Sano, S.",
+            "Otsuka, T.",
+            "Itoyama, K.",
+            "Okuno, H. G."
+        ],
+        "date": "2015",
+        "title": "HMM-based Attacks on Google's ReCAPTCHA with Continuous Visual and Audio Symbols",
+        "publisher": "Journal of Information Processing, 23(6)",
+        "pages": "814-826",
+        "doi": "doi:10.2197/ipsjjip.23.814"
+    },
+    "task-completion": {
+        "authors": [
+            "Sauer, G.",
+            "Lazar, J.",
+            "Hochheiser, H.",
+            "Feng, J."
+        ],
+        "date": "2010",
+        "title": "Towards a universally usable human interaction proof: evaluation of task completion strategies",
+        "publisher": "ACM Transactions on Accessible Computing (TACCESS), 2(4)",
+        "pages": "15"
+    },
+    "captcha-robustness": {
+        "authors": ["Tangmanee, C."],
+        "date": "2016",
+        "title": "Effects of Text Rotation, String Length, and Letter Format on Text-based CAPTCHA Robustness",
+        "publisher": "Journal of Applied Security Research, 11(3)",
+        "pages": "349-361",
+        "doi": "doi:10.1080/19361610.2016.1178553"
+    },
+    "captcha-security": {
+        "authors": [
+            "Yan, J.",
+            "El Ahmad, A. S."
+        ],
+        "date": "2009",
+        "title": "CAPTCHA Security: A Case Study",
+        "publisher": "Security & Privacy, IEEE, 7(4)",
+        "doi": "doi:10.1109/MSP.2009.84"
+    },
+    "mobile-auth": {
+        "authors": [
+            "Yeh, H. T.",
+            "Chen, B. C.",
+            "Wu, Y. C."
+        ],
+        "date": "2013",
+        "title": "Mobile user authentication system in cloud environment",
+        "publisher": "Security and Communication Networks, 6(9)",
+        "pages": "1161-1168",
+        "doi": "doi:10.1002/sec.688"
+    },
+    "game-captcha": {
+        "authors": [
+            "Yang, T.-I.",
+            "Koong, C.-S.",
+            "Tseng, C.-C."
+        ],
+        "date": "2015",
+        "title": "Game-based image semantic CAPTCHA on handset devices",
+        "publisher": "An International Journal, 74(14)",
+        "pages": "5141-5156",
+        "doi": "doi:10.1007/s11042-013-1666-7"
+    }
 }
