7a79bc9ca769b8d3770c428217c3dbe705583d80
Michael Cooper <cooper@w3.org>
2018-06-15 09:47:10 -0400

    add subtitle

--- captcha/respec-config.js
+++ captcha/respec-config.js
@@ -8,6 +8,7 @@ var respecConfig = {
 	permalinkHide:     false,
 	tocIntroductory: true,
 	// specification status (e.g., WD, LC, NOTE, etc.). If in doubt use ED.
+	subtitle: "Alternatives to Visual Turing Tests on the Web",
 	specStatus:           "ED",
 	//crEnd:                "",
 	//perEnd:               "",
