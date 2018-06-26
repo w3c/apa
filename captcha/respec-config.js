e49950b8105705028b75d609d8a95c9cbac16a93
Michael Cooper <cooper@w3.org>
2018-06-26 15:25:06 -0400

    switch to software license since Note track

--- captcha/respec-config.js
+++ captcha/respec-config.js
@@ -21,7 +21,7 @@ var respecConfig = {
 	// if you wish the publication date to be other than today, set this
 	//publishDate:  "2014-12-11",
 	copyrightStart:  "2018",
-	license: "document",
+	license: "w3c-software-doc",
 	
 	// if there is a previously published draft, uncomment this and set its YYYY-MM-DD date
 	// and its maturity status
