5fa7601e80545c3e140a3749a71929a4bfd8cdde
Jason White <jason@jasonjgw.net>
2019-01-18 16:41:06 -0500

    Add bibliographical reference to Marrakesh treaty.

--- biblio.js
+++ biblio.js
@@ -50,7 +50,7 @@ var biblio = {
 	"authors": [
 	    "Silky Azad", "Kiran Jain"],
 	"publisher": "Journal of Computer science and Technology",
-	"etAl": false,
+	"etAl": "false",
 	"editors": [],
 	"href": "https://computerresearch.org/index.php/computer/article/download/368/368"
     },
@@ -67,7 +67,17 @@ var biblio = {
 		"etAl": "false",
 		"publisher": "",
 		"href": "https://www.usenix.org/legacy/events/nsdi05/tech/kandula/kandula_html/"
-	},
+    },
+    "marrakehs": {
+	"title":
+	"Marrakesh Treaty to Facilitate Access to Published Works for Persons Who Are Blind, Visually Impaired or Otherwise Print Disabled",
+	"authors": [],
+	"editors": [],
+	"etAl": "false",
+	"publisher": "World Intellectual Property Organization",
+	"publishDate": "27 June 2013",
+	"href": "https://www.wipo.int/treaties/en/ip/marrakesh"
+    },
 	"newscom": {
 		"title": "Spam-bot tests flunk the blind",
 		"publishDate": "2 July 2003",
