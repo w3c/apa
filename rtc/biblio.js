cf9a673a7b9f52ac2d42f395921731f5a770ef28
RealJoshue108 <joconnor@w3.org>
2020-03-05 11:52:01 +0000

    Biblio file update

---
+++ rtc/biblio.js
@@ -0,0 +1,417 @@
+var biblio = {
+	"aicaptcha": {
+		"title": "aiCaptcha: Using AI to beat CAPTCHA and post comment spam",
+		"date": "",
+		"authors": ["Casey Chesnut"],
+		"editors": [],
+		"etAl": false,
+		"publisher": "",
+		"href": "http://www.brains-n-brawn.com/default.aspx?vDir=aicaptcha"
+	},
+	"antiphishing": {
+		"title": "Phishing Activity Trends Report",
+		"date": "July, 2005",
+		"authors": [],
+		"editors": [],
+		"etAl": false,
+		"publisher": "Anti-Phishing Working Group",
+		"href": "http://antiphishing.org/APWG_Phishing_Activity_Report_Jul_05.pdf"
+	},
+	"breaking": {
+		"title": "Breaking CAPTCHAs Without Using OCR",
+		"date": "",
+		"authors": ["Howard Yeend"],
+		"editors": [],
+		"etAl": false,
+		"publisher": "",
+		"href": "http://www.cs.berkeley.edu/%7Emori/gimpy/gimpy.html"
+	},
+	"breakingocr": {
+		"title": "Breaking CAPTCHAs Without Using OCR",
+		"date": "",
+		"authors": ["Howard Yeend"],
+		"editors": [],
+		"etAl": false,
+		"publisher": "",
+		"href": "http://www.puremango.co.uk/cm_breaking_captcha_115.php"
+	},
+	"chafee": {
+		"title": "17 USC 121, Limitations on exclusive rights: reproduction for blind or other people with disabilities (also known as the Chafee Amendment)",
+		"date": "",
+		"authors": [],
+		"editors": [],
+		"etAl": false,
+		"publisher": "",
+		"href": "https://www.copyright.gov/title17/92chap1.html"
+	},
+    "captcha-ocr": {
+	"title": "CAPTCHA: Attacks and Weaknesses against OCR technology",
+	"date": "2013",
+	"authors": [
+	    "Silky Azad", "Kiran Jain"],
+	"publisher": "Journal of Computer science and Technology",
+	"etAl": false,
+	"editors": [],
+	"href": "https://computerresearch.org/index.php/computer/article/download/368/368"
+    },
+    "information-security": {
+	"title": "Handbook of Information and Communication Security",
+	"authors": [
+	    "Peter Stavroulakis",
+	    "Mark Stamp"],
+	"publisher": "Springer Science &amp; Business Media",
+	"date": "2010"
+    },
+    "kaPoW-plugins": {
+	"title": "kaPoW plugins: protecting web applications using reputation-based proof-of-work",
+	"date": "2012",
+	"authors": [
+	    "Tien Le",
+	    "Akshay Dua",
+	    "Wu-chang Feng"],
+	"editors": [],
+	"etAl": false,
+	"publisher": "Proceedings of the 2nd Joint WICOW/AIRWeb Workshop on Web Quality",
+	"pages": "60-63"
+    },
+    "killbots": {
+		"title": "Botz-4-Sale: Surviving DDos Attacks that Mimic Flash Crowds",
+		"date": "",
+		"authors": [
+			"Srikanth Kandula", 
+			"Dina Katabi", 
+			"Matthias Jacob", 
+			"Arthur Burger"
+		],
+		"editors": [],
+		"etAl": false,
+		"publisher": "",
+		"href": "https://www.usenix.org/legacy/events/nsdi05/tech/kandula/kandula_html/"
+    },
+    "marrakehs": {
+	"title":
+	"Marrakesh Treaty to Facilitate Access to Published Works for Persons Who Are Blind, Visually Impaired or Otherwise Print Disabled",
+	"authors": [],
+	"editors": [],
+	"etAl": false,
+	"publisher": "World Intellectual Property Organization",
+	"date": "27 June 2013",
+	"href": "https://www.wipo.int/treaties/en/ip/marrakesh"
+    },
+	"newscom": {
+		"title": "Spam-bot tests flunk the blind",
+		"date": "2 July 2003",
+		"authors": ["Paul Festa"],
+		"editors": [],
+		"etAl": false,
+		"publisher": "News.com",
+		"href": "https://web.archive.org/web/20030707210529/http://news.com.com/2100-1032-1022814.html"
+	},
+	"pinguard": {
+		"title": "PIN Guard",
+		"date": "",
+		"authors": [],
+		"editors": [],
+		"etAl": false,
+		"publisher": "ING Direct site",
+		"href": "https://secure1.ingdirect.com/tpw/popup_whatIsThis.html"
+	},
+    "privacy-pass": {
+	"title": "Privacy Pass: Bypassing Internet Challenges Anonymously",
+	"date": "2018",
+	"authors": [
+	    "Alex Davidson",
+	    "Ian Goldberg",
+	    "Nick Sullivan",
+	    "George Tankersley",
+	    "Filippo Valsorda"],
+	"editors": [],
+	"etAl": false,
+	"publisher": "Proceedings on Privacy Enhancing technologies; 2018 (3):164-180",
+	"href": "https://www.petsymposium.org/2018/files/papers/issue3/popets-2018-0026.pdf"
+    },
+    "pwntcha": {
+		"title": "PWNtcha - CAPTCHA decoder",
+		"date": "",
+		"authors": ["Sam Hocevar"],
+		"editors": [],
+		"etAl": false,
+		"publisher": "",
+		"href": "http://sam.zoy.org/pwntcha/"
+    },
+    "solving-captchas": {
+	"authors": ["Elie Bursztein"],
+	"etAl": true,
+	"editors": [],
+	"title": "How good are humans at solving CAPTCHAs? A large scale evaluation",
+	"publisher": "2010 IEEE symposium on security and privacy",
+	"date": "2010"
+    },
+        "tls-tracking": {
+	"title": "Exploiting TLS Client Authentication for Widespread User Tracking",
+	"date": "2018",
+	"authors": ["Lucas Foppe",
+		    "Jeremy Martin",
+		    "Travis Mayberry",
+		    "Eric C. Rye",
+		    "Lamont Brown"],
+	"publisher": "Proceedings on Privacy Enhancing Technologies",
+	"etAl": false,
+	"editors": [],
+	"href": "https://www.petsymposium.org/2018/files/papers/issue4/popets-2018-0031.pdf"
+    },
+	"turing": {
+		"title": "The Turing Test",
+		"date": "2002",
+		"authors": [],
+		"editors": [],
+		"etAl": false,
+		"publisher": "he Alan Turing Internet Scrapbook",
+		"href": "http://www.turing.org.uk/turing/scrapbook/test.html"
+	},
+    "eval-audio": {
+        "authors": [
+            "Bigham, J. P.",
+            "Cavender, A. C."
+        ],
+        "date": "April 2009",
+        "title": "Evaluating existing audio CAPTCHAs and an interface optimized for non-visual use",
+        "publisher": "In Proceedings of the SIGCHI Conference on Human Factors in Computing Systems"
+    },
+    "video-events": {
+        "authors": [
+            "Catuogno, L.",
+            "Galdi, C."
+        ],
+        "date": "2014",
+        "title": "On user authentication by means of video events recognition",
+        "publisher": "Journal of Ambient Intelligence and Humanized Computing 5(6)",
+        "pages": "909-918",
+        "doi": "doi:10.1007/s12652-014-0248-5"
+    },
+    "auth-mult": {
+        "authors": ["Cetin, C."],
+        "date": "2015",
+        "title": "Design, Testing and Implementation of a New Authentication Method Using Multiple Devices",
+        "publisher": "J. Ligatti, D. Goldgof, & Y. Liu (Eds.): ProQuest Dissertations Publishing"
+    },
+    "captchastar": {
+        "authors": [
+            "Conti, M.",
+            "Guarisco, C.",
+            "Spolaor, R."
+        ],
+        "date": "2015",
+        "title": "CAPTCHaStar! A novel CAPTCHA based on interactive shape discovery"
+    },
+    "captcha-ld": {
+        "authors": [
+            "Gafni, R.",
+            "Nagar, I."
+        ],
+        "title": "The Effect of CAPTCHA on User Experience among Users with and without Learning Disabilities"
+    },
+    "civil-rights-captcha": {
+        "authors": [
+            "Hernández-Castro, C. J.",
+            "Barrero, D. F.",
+            "R-Moreno, M. D."
+        ],
+        "date": "2016",
+        "title": "Machine learning and empathy: the Civil Rights CAPTCHA",
+        "publisher": "Concurrency and Computation: Practice and Experience, 28(4)",
+        "pages": "1310-1323",
+        "doi": "doi:10.1002/cpe.3632"
+    },
+    "iso-8859-1": {
+    	"title": "Information technology -- 8-bit single-byte coded graphic character sets -- Part 1: Latin alphabet No. 1",
+    	"href": "https://www.iso.org/standard/28245.html",
+    	"publisher": "International Organization for Standardization",
+    	"date": "1998"
+    },
+    "facecaptcha": {
+        "authors": [
+            "Kim, J.",
+            "Kim, S.",
+            "Yang, J.",
+            "Ryu, J.-h.",
+            "Wohn, K."
+        ],
+        "date": "2014",
+        "title": "FaceCAPTCHA: a CAPTCHA that identifies the gender of face images unrecognized by existing gender classifiers",
+        "publisher": "An International Journal, 72(2)",
+        "pages": "1215-1237. ",
+        "doi": "doi:10.1007/s11042-013-1422-z"
+    },
+    "video-captcha-security": {
+        "authors": ["Kluever, K."],
+        "date": "2008",
+        "title": "Evaluating the usability and security of a video CAPTCHA",
+        "publisher": "R. Zanibbi, Z. Butler, & R. Canosa (Eds.): ProQuest Dissertations Publishing."
+    },
+    "social-classification": {
+        "authors": ["Korayem, M."],
+        "date": "2015",
+        "title": "Social and egocentric image classification for scientific and privacy applications",
+        "publisher": "D. Crandall, J. Bollen, A. Kapadia, & P. Radivojac (Eds.): ProQuest Dissertations Publishing."
+    },
+    "facial-captcha-attack": {
+        "authors": ["Li, Q."],
+        "date": "2015",
+        "title": "A computer vision attack on the ARTiFACIAL CAPTCHA",
+        "publisher": "An International Journal, 74(13)",
+        "pages": "4583-4597",
+        "doi": "doi:10.1007/s11042-013-1823-z"
+    },
+    "defeat-line-noise": {
+        "authors": [
+            "Nakaguro, Y.",
+            "Dailey, M. N.",
+            "Marukatat, S.",
+            "Makhanov, S. S."
+        ],
+        "date": "2013",
+        "title": "Defeating line-noise CAPTCHAs with multiple quadratic snakes",
+        "publisher": "Computers & Security, 37",
+        "pages": "91-110",
+        "doi": "doi:10.1016/j.cose.2013.05.003"
+    },
+    "3d-captcha-security": {
+        "authors": [
+            "Nguyen, V. D.",
+            "Chow, Y.-W.",
+            "Susilo, W."
+        ],
+        "date": "2014",
+        "title": "On the security of text-based 3D CAPTCHAs",
+        "publisher": "Computers & Security, 45",
+        "pages": "84-99",
+        "doi": "doi:10.1016/j.cose.2014.05.004"
+    },
+    "recaptcha": {
+    	"title": "reCAPTCHA",
+    	"publisher": "Google",
+    	"href": "https://www.google.com/recaptcha/"
+    },
+    "recaptcha-attacks": {
+        "authors": [
+            "Sano, S.",
+            "Otsuka, T.",
+            "Itoyama, K.",
+            "Okuno, H. G."
+        ],
+        "date": "2015",
+        "title": "HMM-based Attacks on Google's ReCAPTCHA with Continuous Visual and Audio Symbols",
+        "publisher": "Journal of Information Processing, 23(6)",
+        "pages": "814-826",
+        "doi": "doi:10.2197/ipsjjip.23.814"
+    },
+    "task-completion": {
+        "authors": [
+            "Sauer, G.",
+            "Lazar, J.",
+            "Hochheiser, H.",
+            "Feng, J."
+        ],
+        "date": "2010",
+        "title": "Towards a universally usable human interaction proof: evaluation of task completion strategies",
+        "publisher": "ACM Transactions on Accessible Computing (TACCESS), 2(4)",
+        "pages": "15"
+    },
+    "captcha-robustness": {
+        "authors": ["Tangmanee, C."],
+        "date": "2016",
+        "title": "Effects of Text Rotation, String Length, and Letter Format on Text-based CAPTCHA Robustness",
+        "publisher": "Journal of Applied Security Research, 11(3)",
+        "pages": "349-361",
+        "doi": "doi:10.1080/19361610.2016.1178553"
+    },
+    "captcha-security": {
+        "authors": [
+            "Yan, J.",
+            "El Ahmad, A. S."
+        ],
+        "date": "2009",
+        "title": "CAPTCHA Security: A Case Study",
+        "publisher": "Security & Privacy, IEEE, 7(4)",
+        "doi": "doi:10.1109/MSP.2009.84"
+    },
+    "36-cfr-1194": {
+    	"title": "36 CFR Appendix C to Part 1194, Functional Performance Criteria and Technical Requirements",
+    	"href": "https://www.law.cornell.edu/cfr/text/36/appendix-C_to_part_1194",
+    	"publisher": "Legal Information Institute"
+    },
+    "en-301-549": {
+    	"title": "EN 301 549: Accessibility requirements suitable for public procurement of ICT products and services in Europe",
+    	"publisher": "CEN/CENELEC/ETSI",
+    	"href": "http://mandate376.standards.eu/standard"
+    },
+    "mobile-auth": {
+        "authors": [
+            "Yeh, H. T.",
+            "Chen, B. C.",
+            "Wu, Y. C."
+        ],
+        "date": "2013",
+        "title": "Mobile user authentication system in cloud environment",
+        "publisher": "Security and Communication Networks, 6(9)",
+        "pages": "1161-1168",
+        "doi": "doi:10.1002/sec.688"
+    },
+    "game-captcha": {
+        "authors": [
+            "Yang, T.-I.",
+            "Koong, C.-S.",
+            "Tseng, C.-C."
+        ],
+        "date": "2015",
+        "title": "Game-based image semantic CAPTCHA on handset devices",
+        "publisher": "An International Journal, 74(14)",
+        "pages": "5141-5156",
+        "doi": "doi:10.1007/s11042-013-1666-7"
+    },
+    "marrakesh": {
+    	"date": "27 June 2013",
+    	"href": "https://www.wipo.int/treaties/en/ip/marrakesh",
+    	"publisher": "World Intellectual Property Organization",
+    	"title": "Marrakesh Treaty to Facilitate Access to Published Works for Persons Who Are Blind, Visually Impaired or Otherwise Print Disabled"
+    },
+    "ietf-rtc": {
+            "date": "March 2015",
+            "href": "https://tools.ietf.org/html/rfc7478",
+            "publisher": "IETF",
+            "title": "Web Real-Time Communication Use Cases and Requirements"
+    },
+    "webrtc-use-cases": {
+        "date": "11 December 2018",
+        "href": "https://www.w3.org/TR/webrtc-nv-use-cases/",
+        "publisher": "W3C",
+        "title": "WebRTC Next Version Use Cases"
+    },
+    "rtt-sip": {
+        "date": "June 2008",
+        "href": "https://tools.ietf.org/html/rfc5194",
+        "publisher": "IETF, Network Working Group",
+        "title": "Framework for Real-Time Text over IP Using the Session Initiation Protocol (SIP)"
+    },
+    "EN301-549": {
+        "date": "August 2018",
+        "href": "http://mandate376.standards.eu/standard",
+        "publisher": "CEN/CENELEC/ETSI",
+        "title": "Accessibility requirements suitable for public procurement of ICT products and services in Europe"
+    },
+    "webrtc-priority": {
+        "date": "12 February 2020",
+        "href": "https://w3c.github.io/webrtc-priority/",
+        "publisher": "W3C",
+        "title": "WebRTC DSCP Control API"
+    },
+
+     "ietf-relay": {
+        "date": "August 5, 2019",
+        "href": "https://tools.ietf.org/html/draft-rosen-rue-00/",
+        "publisher": "IETF",
+        "title": "Interoperability Profile for Relay User EquipmentPI"
+    }
+                          
+}
