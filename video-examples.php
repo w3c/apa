b26fed56c3a2150b3e021497fc218d641b5d5ae6
Becky Gibson <becky.gibson@gmail.com>
2021-04-17 13:59:26 -0400

    first draft at video examples page

---
+++ video-examples.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" dir="ltr">
+	<head>
+		<?php include "_head.phi"; ?>
+		<title>APA Video Examples</title>
+	</head>
+	<body>
+		<?php include "_header.phi"; ?>
+		<h1>APA Video Examples</h1>
+		<p>The following videos and highlight the work being performed within our task forces. </p>
+		<section>
+		<h2>Personalization Video Example</h2>
+		<p>The <a href="https://www.w3.org/WAI/APA/task-forces/personalization/">Personalization Task Force</a> created a video to demonstrate a semantic overlay approach to enable user driven personalization, eg. the association of user-preferred symbols with elements having those semantics: <a href="https://www.w3.org/2020/10/TPAC/apa-personalization.html"><strong>Web Support for People with Cognitive and Learning Disabilities</strong>.</a></p>
+		</section>
+		<section>
+		<h2>Pronunciation Video Example</h2>
+		<p> The <a href="https://www.w3.org/WAI/APA/task-forces/pronunciation/"> Spoken Pronunciation Task Force</a> created a video to demonstrate proposed approaches for controlling pronunciation in spans of web content: <a href="https://www.w3.org/2020/10/TPAC/apa-pronunciation.html"><strong>Pronunciation Matters</strong></a>.
+		</section>
+		<section>
+		<h2>CSS Media Queries 5 and Personalization - March 10, 2021</h2>
+			<p>A special presentation by Niklaus Schildhauer on <strong>CSS Media Queries 5 Opportunities for Personalization and Implementation as Polyfill</strong>. This was Nicklaus' Bachelor thesis for Mobile Media at the <a href="https://www.hdm-stuttgart.de/en">Stuttgart Media Univerity.</a> He was advised by Professor Dr. Gottfried Zimmermann.</p>
+		<h3>Video Recording</h3>
+			<p>embedded video or video link goes here</p>
+		<h3>Materials</h3>
+		<ul>
+			<li>Niklaus has shared his work via gitlab at <a href="https://gitlab.mi.hdm-stuttgart.de/ns107/ba-mq5">https://gitlab.mi.hdm-stuttgart.de/ns107/ba-mq5 </a></li>
+			<li><a href="https://gitlab.mi.hdm-stuttgart.de/ns107/ba-mq5/-/blob/master/docs/1_Presentation/20201118_Common_Terms.pptx">Download his PowerPoint Presenatation</a> from the gitlab site.</li>
+			<li>Links to Examples:
+				<ul>
+					<li><a href="https://gpii.eu/mq-5/example/video/"> Video Streaming Website</a></li>
+					<li><a href="https://gpii.eu/mq-5/example/text/"> Text Website Example</a></li>
+					<li><a href="https://gpii.eu/mq-5/example/registration/"> Registration Example</a></li>
+				</ul>
+			</li>
+		</ul>
+		</section>
+		<?php include "_footer.phi"; ?>
+	</body>
+</html>
