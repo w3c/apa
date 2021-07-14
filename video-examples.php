763edb8637845979187b0fba0265160aaf85c5e4
Michael Cooper <cooper@w3.org>
2021-07-14 12:07:47 -0400

    Update video-examples.php

--- video-examples.php
+++ video-examples.php
@@ -10,28 +10,50 @@
 		<p>The following videos highlight the work being performed in APA and within our task forces. </p>
 		<section>
 		<h2>Personalization Video Example</h2>
-		<p>The <a href="https://www.w3.org/WAI/APA/task-forces/personalization/">Personalization Task Force</a> created a video to demonstrate a semantic overlay approach to enable user driven personalization, eg. the association of user-preferred symbols with elements having those semantics: <a href="https://www.w3.org/2020/10/TPAC/apa-personalization.html"><strong>Web Support for People with Cognitive and Learning Disabilities</strong>.</a></p>
+		<p>The <a href="https://www.w3.org/WAI/APA/task-forces/personalization/">Personalization Task Force</a> created a video to demonstrate a semantic overlay approach to enable user driven personalization, eg. the association of user-preferred symbols with elements having those semantics.</p>
+		<h3>Video Recording</h3>
+		<p><strong>Web Support for People with Cognitive and Learning Disabilities</strong></p>
+		<iframe
+		     width="640" height="360"
+		     src="https://player.streamfizz.live/embed/media/ckfigusqfihig0731jmex8a9s"
+		     frameborder="0"
+		     allow="accelerometer; autoplay; encrypted-media; picture-in-picture" allowFullScreen="">
+		  </iframe>
 		</section>
 		<section>
 		<h2>Pronunciation Video Example</h2>
-		<p> The <a href="https://www.w3.org/WAI/APA/task-forces/pronunciation/"> Spoken Presentation Task Force</a> created a video to demonstrate proposed approaches for controlling pronunciation in spans of web content: <a href="https://www.w3.org/2020/10/TPAC/apa-pronunciation.html"><strong>Pronunciation Matters</strong></a>.</p>
+		<p> The <a href="https://www.w3.org/WAI/APA/task-forces/pronunciation/">Spoken Presentation Task Force</a> created a video to demonstrate proposed approaches for controlling pronunciation in spans of web content. 
+		<h3>Video Recording</h3>
+		<p><strong>Pronunciation Matters</strong></p>
+		<iframe
+		     width="640" height="360"
+		     src="https://player.streamfizz.live/embed/media/ckfibm5fbh2xp0731w0leedro"
+		     frameborder="0"
+		     allow="accelerometer; autoplay; encrypted-media; picture-in-picture" allowFullScreen="">
+		  </iframe>
 		</section>
 		<section>
 		<h2>CSS Media Queries 5 and Personalization - March 10, 2021</h2>
 			<p>A special presentation by Niklaus Schildhauer on <strong>CSS Media Queries 5 Opportunities for Personalization and Implementation as Polyfill</strong>. This was Nicklaus' Bachelor thesis for Mobile Media at the <a href="https://www.hdm-stuttgart.de/en">Stuttgart Media University.</a> He was advised by Professor Dr. Gottfried Zimmermann.</p>
 		<h3>Video Recording</h3>
-			<p>embedded video or video link goes here</p>
+		  <iframe
+		     width="640" height="360"
+		     src="https://player.streamfizz.live/embed/media/ckq9ijuy7456134axkz0pojtlnc"
+		     frameborder="0"
+		     allow="accelerometer; autoplay; encrypted-media; picture-in-picture" allowFullScreen="">
+		  </iframe>
 		<h3>Materials</h3>
 		<ul>
 			<li>Niklaus has shared his work via gitlab at <a href="https://gitlab.mi.hdm-stuttgart.de/ns107/ba-mq5">https://gitlab.mi.hdm-stuttgart.de/ns107/ba-mq5 </a></li>
 			<li><a href="https://gitlab.mi.hdm-stuttgart.de/ns107/ba-mq5/-/blob/master/docs/1_Presentation/20201118_Common_Terms.pptx">Download his PowerPoint Presentation</a> from the gitlab site.</li>
 			<li>Links to Examples:
 				<ul>
-					<li><a href="https://gpii.eu/mq-5/example/video/"> Video Streaming Website</a></li>
-					<li><a href="https://gpii.eu/mq-5/example/text/"> Text Website Example</a></li>
-					<li><a href="https://gpii.eu/mq-5/example/registration/"> Registration Example</a></li>
+					<li><a href="https://gpii.eu/mq-5/example/video/">Video Streaming Website</a></li>
+					<li><a href="https://gpii.eu/mq-5/example/text/">Text Website Example</a></li>
+					<li><a href="https://gpii.eu/mq-5/example/registration/">Registration Example</a></li>
 				</ul>
 			</li>
+			<p>Note: Adopting Niklaus' additional Media Queries features will require us to resolve <a href="https://github.com/w3c/csswg-drafts/issues/3488">CSS Issue 3488</a>.</p>
 		</ul>
 		</section>
 		<?php include "_footer.phi"; ?>
