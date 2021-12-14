24b39a64368093f5418d673e586005a041decb74
RealJoshue108 <joconnor@w3.org>
2021-12-14 16:42:05 +0000

    New biblio.js file with academic references etc for SAUR

---
+++ saur/new_biblio.js
@@ -0,0 +1,703 @@
+respecConfig.localBiblio = {
+
+	"neural-synchrony": {
+		"authors" : [
+		"Alho, J",
+		"  Lin, F",
+		" H., Sato",
+		"M., Tiitinen ",
+		"H., Sams",
+		"M., & Jääskeläinen"
+		],
+		"date": "2014",
+		"title": "Enhanced neural synchrony between left auditory and premotor cortex is associated with successful phonetic categorization.",
+		"publishers": "Frontiers in Psychology, ",
+		"pages": " 5, 394"
+	},
+
+		"live-tv-subs": {
+		"authors" : [
+		"Armstrong, M."
+		],
+		"date": "Oct 2013",
+		"title": "The Development of a Methodology to Evaluate the Perceived Quality of Live TV Subtitles",
+		"publishers": "BBC Research & Development White Paper WHP",
+		"pages": "259"
+
+	},
+
+	"imperfect-auto-speech": {
+		"authors" : [
+		"Berke, L. ",
+		"Caulfield, C.",
+		"Huenerfauth, M"
+		],
+		"date": "Oct 2017",
+		"title": "Deaf and hard-of-hearing perspectives on imperfect automatic speech recognition for captioning one-on-one meetings",
+		"publishers": "In Proceedings of the 19th International ACM SIGACCESS Conference on Computers and Accessibility",
+		"pages": "155-164"
+
+	},
+
+		"sync-survey": {
+		"authors" : [
+		"Blakowski, G. ",
+		"Steinmetz, R "
+		],
+		"date": " ",
+		"title": "A media synchronization survey: Reference model, specification, and case studies",
+		"publishers": "IEEE journal on selected areas in communications",
+		"pages": "14(1),5-35"
+
+	},
+
+		"sign-tv": {
+		"authors" : [
+		"Bosch-Baliarda, Marta",
+		"Olga Soler-Vilageliu",
+		"Pilar Orero"
+		],
+		"date": "2020",
+		"title": "Sign language interpreting on TV: a reception study of visual screen exploration in deaf signing users",
+		"publishers": " In: Richart-Marset, Mabel & Francesca Calamita (eds.) 2020. Traducción y Accesibilidad en los medios de comunicación: de la teoría a la práctica / Translation and Media Accessibility: from Theory to Practice.",
+		"pages": "MonTI 12, pp. 108-143."
+
+	},
+
+		"canadian-network": {
+		"authors" : [
+		"Boyce, M.",
+		"Diamond, S.",
+		"Fels, D.",
+		"Gadsby, E.",
+		"Harvie, R.",
+		"Porch, W.",
+		"Treviranus, J"
+		],
+		"date": "Boyce, M. ",
+		"title": "Canadian Network for Inclusive Cultural Exchange",
+		"publishers": "CNICE"
+	
+
+	},
+
+		"sign-recog": {
+		"authors" : [
+		"Bragg, D ",
+		"Koller, O. ",
+		"Bellard, M. ",
+		"Berke, L",
+		"Boudreault, P.",
+		"Braffort, A.",
+		"Ringel Morris, M"
+		],
+		"date": " ",
+		"title": "Sign language recognition, generation, and translation: An interdisciplinary perspective",
+		"publishers": "21st International ACM SIGACCESS Conference on Computers and Accessibility ",
+		"pages": "pp. 16-31"
+
+	},
+
+		"captions-time": {
+		"authors" : [
+		"Burnham, D ",
+		"Robert-Ribes, J ",
+		"Ellison, R "
+		],
+		"date": "1998",
+		"title": "Why captions have to be on time ",
+		"publishers": "In AVSP'98 International Conference on Auditory-Visual Speech Processing "
+	
+
+	},
+
+		"lip-sync": {
+		"authors" : [
+		"Chen, M "
+		],
+		"date": "April 2003 ",
+		"title": "A low-latency lip-synchronized videoconferencing system ",
+		"publishers": "In Proceedings of the SIGCHI Conference on Human Factors in Computing Systems ",
+		"pages": "pp. 465-471 "
+
+	},
+			"integration-multi": {
+		"authors" : [
+		"Chen, T. ",
+		"Rao, R. R. "
+		],
+		"date": "1998",
+		"title": "Audio-visual integration in multimodal communication",
+		"publishers": " Proceedings of the IEEE, 86(5)",
+		"pages": "837-852 "
+
+	},
+			"lag-effect": {
+		"authors" : [
+		"Cokely, D "
+		],
+		"date": "1986",
+		"title": "The effects of lag time on interpreter errors ",
+		"publishers": "Sign Language Studies ",
+		"pages": "341-375 "
+
+	},
+			"dev-video": {
+		"authors" : [
+		"Cronin, B. J. ",
+		"King, S. R "
+		],
+		"date": "1990 ",
+		"title": "The Development of the Descriptive Video Services ",
+		"publishers": "Journal of Visual Impairment & Blindness, 84(10), ",
+		"pages": "503-506 "
+
+	},
+			"embed-sign": {
+		"authors" : [
+		"De Araújo, T. M. U.",
+		"Ferreira, F. L.",
+		"Silva, D. A. ",
+		"Oliveira, L. D",
+		"Falcão, E. L.",
+		"Domingues, L. A",
+		"Duarte"
+		],
+		"date": "2014 ",
+		"title": "An approach to generate and embed sign language video tracks into multimedia contents.",
+		"publishers": "Information Sciences, 281",
+		"pages": "762-780 "
+
+	},
+			"media-for-all": {
+		"authors" : [
+		"Díaz-Cintas, J. ",
+		"Orero, P. ",
+		"Remael, A "
+		],
+		"date": "2007 ",
+		"title": "Media for all: subtitling for the deaf, video description, and sign language (Vol. 30) ",
+		"publishers": "Rodopi "
+	
+
+	},
+			"audio-live": {
+		"authors" : [
+		"Di Giovanni, E ",
+		" ",
+		" "
+		],
+		"date": "2018 ",
+		"title": "Audio description for live performances and audience participation ",
+		"publishers": "Jostrans: the Journal of Specialised Translation, 29 ",
+		"pages": "189-211 "
+
+	},
+			"auto-speech": {
+		"authors" : [
+		"Errattahi, R. ",
+		"El Hannani, A. ",
+		"Ouahmane, H "
+		],
+		"date": "2018 ",
+		"title": "Automatic speech recognition errors detection and correction: A review ",
+		"publishers": "Procedia Computer Science, 128 ",
+		"pages": "32-37 "
+
+	},
+			"fcc-auto-speech": {
+		"authors" : [
+		"Federal Communications Commission ",
+		" ",
+		" "
+		],
+		"date": "2018 ",
+		"title": "Declaratory Ruling on Automatic Speech Recognition ",
+		"publishers": "In Report and Order, Declaratory Ruling, Further Notice of Proposed Rulemaking, and Notice of Inquiry (33 FCC Rcd 5800 (9), 5827 ",
+		"pages": " para. 48"
+
+	},
+			"lip-sync-video": {
+		"authors" : [
+		"Firestone, S ",
+		" ",
+		" "
+		],
+		"date": "2007 ",
+		"title": "Lip Synchronization in Video Conferencing ",
+		"publishers": "Cisco Systems Ltd - Voice and Video Conferencing Fundamentals "
+	
+
+	},
+
+
+		"sync-tv-news": {
+		"authors" : [
+		"Garcia, J. E. ",
+		"Ortega, A. ",
+		"Lleida, E. ",
+		"Lozano, T.",
+		"Bernues, E.",
+		"Sanchez, D"
+		],
+		"date": "May 2009 ",
+		"title": "Audio and text synchronization for TV news subtitling based on automatic speech recognition ",
+		"publishers": "In 2009 IEEE International Symposium on Broadband Multimedia Systems and Broadcasting ",
+		"pages": "pp. 1-6 "
+
+	},
+
+		"speech-intel": {
+		"authors" : [
+		"Grant, K. W. ",
+		"Greenberg, S ",
+		" "
+		],
+		"date": "2001 ",
+		"title": "Speech intelligibility derived from asynchronous processing of auditory-visual information ",
+		"publishers": "In AVSP 2001-International Conference on Auditory-Visual Speech Processing "
+	
+
+	},
+
+		"sign-relay": {
+		"authors" : [
+		"Han, H. H. ",
+		"Yu, H. N ",
+		" "
+		],
+		"date": "2020 ",
+		"title": "An empirical study of temporal variables and their correlations in spoken and sign language relay interpreting ",
+		"publishers": "Babel, 66(4-5)",
+		"pages": "619-635 "
+
+	},
+
+		"closed-caption": {
+		"authors" : [
+		"Huang, C. W ",
+		"Hsu, W. ",
+		"Chang, S. F "
+		],
+		"date": "2003",
+		"title": "Automatic closed caption alignment based on speech recognition transcripts ",
+		"publishers": "Rapport technique, Columbia "
+	
+
+	},
+
+		"app-profile": {
+		"authors" : [
+		"ITU-T ",
+		"S. H ",
+		" "
+		],
+		"date": "1999 ",
+		"title": "Application profile–Sign language and lip-reading real-time conversation using low bit-rate video communication ",
+		"publishers": "CCITT Recommendations "
+	
+
+	},
+
+		"multimodal-speech": {
+		"authors" : [
+		"Ivanko, D. ",
+		"Karpov, A. ",
+		"Fedotov, D. ",
+		"Kipyatkova, I.",
+		"Ryumin, D.",
+		"Ivanko, D.",
+		"Zelezny, M"
+		],
+		"date": "2018",
+		"title": "Multimodal speech recognition: increasing accuracy using high speed video data ",
+		"publishers": "Journal on Multimodal User Interfaces, 12(4) ",
+		"pages": " 319-328 "
+
+	},
+
+		"filmic-audio": {
+		"authors" : [
+		"Jankowska, A. ",
+		"ZióŁko, B. ",
+		"Igras-Cybulska, M ",
+		" Psiuk, A."
+		],
+		"date": "2017 ",
+		"title": "Reading rate in filmic audio description. Rivista Internazionale di Tecnica della Traduzione ",
+		"publishers": "International Journal of Translation, 19"
+	
+
+	},
+
+		"cart-cc": {
+		"authors" : [
+		"Jiline, M ",
+		"Kirk, D. ",
+		"Quirk, K ",
+		" Sandler, M.",
+		"Monette, M"
+		],
+		"date": "2020 ",
+		"title": " A Review Of State-Of-The-Art Automatic Speech Recognition Services For CART And CC Applications ",
+		"publishers": "Proceedings of the 2020 NAB Broadcast Engineering and Information Technology (BEIT) Conference "
+	
+
+	},
+
+		"distance-effects": {
+		"authors" : [
+		"Jordan, T. R ",
+		"Sergeant, P. ",
+		" "
+		],
+		"date": "2000 ",
+		"title": "Effects of distance on visual and audiovisual speech recognition ",
+		"publishers": "Language and Speech, 43(1) ",
+		"pages": " 107-124 "
+
+	},
+
+	"usability-captions": {
+		"authors" : [
+		"Kafle, S. ",
+		"Huenerfauth, M "
+		],
+		"date": "Oct 2017",
+		"title": "Evaluating the usability of automatically generated captions for people who are deaf or hard of hearing ",
+		"publishers": "In Proceedings of the 19th International ACM SIGACCESS Conference on Computers and Accessibility  ",
+		"pages": "pp. 165-174 "
+
+	},
+
+	"matching-speech": {
+		"authors" : [
+		"Kaganovich, N. ",
+		"Schumaker, J. ",
+		"Rowland, C "
+		],
+		"date": "2016 ",
+		"title": "Matching heard and seen speech: An ERP study of audiovisual word recognition ",
+		"publishers": "Brain and language, 157-158 ",
+		"pages": "14–24 "
+
+	},
+
+	"asl-xr": {
+		"authors" : [
+		"Keating, E. ",
+		"Mirus, G "
+		],
+		"date": "2003",
+		"title": "American Sign Language in virtual space: Interactions between deaf users of computer-mediated video communication and the impact of technology on language practices ",
+		"publishers": "Language in Society ",
+		"pages": "693-714 "
+
+	},
+
+	"survey-sign": {
+		"authors" : [
+		"Koller, O. ",
+		" ",
+		" "
+		],
+		"date": " 2020",
+		"title": "Quantitative survey of the state of the art in sign language recognition ",
+		"publishers": "arXiv preprint arXiv:2008.09918"
+	
+
+	},
+
+	"sync-xr": {
+		"authors" : [
+		"Kumar, P. J. ",
+		"Hu, W. ",
+		"Yung, Y "
+		],
+		"date": "2017 ",
+		"title": "Virtual Reality Based 3D Video Games and Speech-Lip Synchronization Superseding Algebraic Code Excited Linear Prediction ",
+		"publishers": "International Journal of Computer and Information Sciences, 4(12) ",
+		"pages": " 303-321 "
+
+	},
+
+	"timing-captions": {
+		"authors" : [
+		"Maruyama, I. ",
+		"Abe, Y ",
+		"Sawamura, E. ",
+		"Mitsuhashi, T",
+		"Ehara, T.",
+		"Shirai, K"
+		],
+		"date": "1999 ",
+		"title": "Cognitive experiments on timing lag for superimposing closed captions ",
+		"publishers": " In Sixth European Conference on Speech Communication and Technology. "
+	
+
+	},
+
+	"dyslexia-web": {
+		"authors" : [
+		"McCarthy, J. E. ",
+		"Swierenga, S. J ",
+		" "
+		],
+		"date": "2010",
+		"title": "What we know about dyslexia and web accessibility: a research review ",
+		"publishers": " Universal Access in the Information Society, 9(2) ",
+		"pages": "147-152 "
+
+	},
+
+	"caption-quality": {
+		"authors" : [
+		"Mikul, C ",
+		" ",
+		" "
+		],
+		"date": "2014 ",
+		"title": "Caption quality: Approaches to standards and measurement",
+		"publishers": "Media Access Australia"
+	
+
+	},
+
+	"media-sync": {
+		"authors" : [
+		"Montagud, M. ",
+		"Cesar, P. ",
+		"Boronat, F. ",
+		" Jansen, J"
+		],
+		"date": " 2018",
+		"title": "Introduction to media synchronization (MediaSync) ",
+		"publishers": "In MediaSync, Springer, Cham  ",
+		"pages": "pp. 3-31 "
+
+	},
+
+	"prediction-const": {
+		"authors" : [
+		"Peelle, J. E. ",
+		"Sommers, M. S "
+		],
+		"date": " 2015",
+		"title": "Prediction and constraint in audiovisual speech perception ",
+		"publishers": "Cortex; a journal devoted to the study of the nervous system and behavior, 68 ",
+		"pages": "169–181"
+
+	},
+		"interaction-media": {
+		"authors" : [
+		"Petrie, H. L. ",
+		"Weber, G. ",
+		"Fisher, W "
+		],
+		"date": " 2005",
+		"title": "Personalization, interaction, and navigation in rich multimedia documents for print-disabled users ",
+		"publishers": "IBM Systems Journal, 44(3) ",
+		"pages": "629-635 "
+
+	},
+		"lang-system": {
+		"authors" : [
+		"Piety, P. J ",
+		" ",
+		" "
+		],
+		"date": "2004 ",
+		"title": "The language system of audio description: an investigation as a discursive process ",
+		"publishers": "Journal of Visual Impairment & Blindness, 98(8) ",
+		"pages": "453-469 "
+
+	},
+		"display-rate": {
+		"authors" : [
+		"Sandford, J. "
+		],
+		"date": "2015",
+		"title": "The impact of subtitle display rate on enjoyment under normal television viewing conditions ",
+		"publishers": "BBC Research & Development White Paper WHP  ",
+		"pages": "306 "
+
+	},
+		"reading-rates": {
+		"authors" : [
+		"Shroyer, E. H. ",
+		"Birch, J "
+		],
+		"date": "1980 ",
+		"title": "Captions and reading rates of hearing-impaired students ",
+		"publishers": "American Annals of the Deaf, 125(7), ",
+		"pages": "916-922 "
+
+	},
+		"simul-trans-video": {
+		"authors" : [
+		"Staelens ",
+		"Nicolas & De Meulenaere",
+		"Jonas & Bleumers",
+		"Lizzy & Wallendael et al"
+		],
+		"date": "2012",
+		"title": " Assessing the importance of audio/video synchronization for simultaneous translation of video sequences ",
+		"publishers": " Multimedia Systems. 18."
+	
+
+	},
+		"speech-intel-noise": {
+		"authors" : [
+		"Sumby, W. H ",
+		"Pollack, I "
+		],
+		"date": "1954 ",
+		"title": "Visual contribution to speech intelligibility in noise ",
+		"publishers": "The journal of the acoustical society of america, 26(2), ",
+		"pages": "212-215 "
+
+	},
+		"lip-speech": {
+		"authors" : [
+		"Summerfield, Q "
+		],
+		"date": "1992 ",
+		"title": "Lipreading and audio-visual speech perception ",
+		"publishers": "Philosophical Transactions of the Royal Society of London. Series B: Biological Sciences, 335(1273) ",
+		"pages": "71-78"
+
+	},
+		"timing-audiovisual": {
+		"authors" : [
+		"Venezia, J.H. ",
+		"Thurman, S.M. ",
+		"Matchin, W. et al "
+		],
+		"date": "2016 ",
+		"title": "Timing in audiovisual speech perception: A mini review and new psychophysical data ",
+		"publishers": "Attention, Perception, & Psychophysics, 78 ",
+		"pages": "583–601 "
+
+	},
+		"auto-sync": {
+		"authors" : [
+		"Waters, K. ",
+		"Levergood, T "
+		],
+		"date": "1994",
+		"title": "An automatic lip-synchronization algorithm for synthetic faces ",
+		"publishers": "In Proceedings of The second ACM international conference on Multimedia  ",
+		"pages": "pp. 149-156 "
+
+	},
+		"gaze-patterns": {
+		"authors" : [
+		"Yi, A ",
+		"Wong, W.",
+		"Eizenman, M "
+		],
+		"date": "2013 ",
+		"title": "Gaze patterns and audiovisual speech enhancement",
+		"publishers": "Journal of Speech, Language, and Hearing Research "
+	
+
+	},
+		"sync-delays": {
+		"authors" : [
+		"Ziegler, C. ",
+		"Keimel, C. ",
+		"Ramdhany, R. ",
+		"Vinayagamoorthy, V"
+		],
+		"date": "2017",
+		"title": "On time or not on time: A user study on delays in a synchronised companion-screen experience ",
+		"publishers": "In Proceedings of the 2017 ACM International Conference on Interactive Experiences for TV and Online Video ",
+		"pages": "pp. 105-114"
+
+	},
+	
+
+
+"en-301-549": {
+		"title": "EN 301 549 v3.2.1: Harmonised European Standard - Accessibility requirements for ICT products and services",
+		"publisher": "CEN/CENELEC/ETSI",
+		"date": "2021-03",
+		"href": "https://www.etsi.org/deliver/etsi_en/301500_301599/301549/03.02.01_60/en_301549v030201p.pdf"
+	},
+
+	"media-accessibility-reqs": {
+		"title": "Media Accessibility User Requirements",
+		"authors": [
+			"Shane McCarron",
+			"Michael Cooper",
+			"Mark Sadecki"
+		],
+		"date": "3 December 2015",
+		"publisher": "W3C",
+		"href": "https://www.w3.org/TR/media-accessibility-reqs/"
+	},
+
+	    "mitre-ipcaption": {
+        "date": "23 March 2016",
+        "title": "Internet Protocol Caption Telephone Service (IP CTS) – Summary of Phase 2 Usability Testing Results",
+        "publisher": "MITRE Corporation",
+      	"href": "https://ecfsapi.fcc.gov/file/10411287298464/MITRE%20Corporation%20Summary%20of%20Phase%202.pdf"
+    },
+
+	"raur": {
+		"date": "25 May 2021",
+		"href": "https://www.w3.org/TR/raur/",
+		"publisher": "W3C",
+		"title": "RTC Accessibility User Requirements",
+		"authors": [
+			"Joshue O'Connor",
+			"Janina Sajka",
+			"Jason White",
+			"Michael Cooper"
+		]
+	},
+
+
+"wcag21":{
+	"date": "5 June 2018",
+	"href": "https://www.w3.org/TR/WCAG21/",
+		"publisher": "W3C",
+		"title": "Web Content Accessibility Guidelines (WCAG) 2.1",
+		"authors": [
+			"Andrew Kirkpatrick",
+			"Joshue O'Connor",
+			"Alastair Campbell",
+			"Michael Cooper"
+		]
+
+},
+
+	"xaur": {
+		"date": "16 Sept 2020",
+		"href": "https://www.w3.org/TR/xaur/",
+		"publisher": "W3C",
+		"title": "XR Accessibility User Requirements",
+		"authors": [
+			"Joshue O'Connor",
+			"Janina Sajka",
+			"Jason White",
+			"Scott Hollier",
+			"Michael Cooper"
+		]
+	}
+
+
+	"short-name": {
+		"authors" : [
+		" ",
+		" ",
+		" "
+		],
+		"date": " ",
+		"title": " ",
+		"publishers": " ",
+	
+
+	}
+
+	};
