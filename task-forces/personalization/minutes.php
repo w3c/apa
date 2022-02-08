74b9e199a739fd5ca25c24720ddf5874ff6b4be5
Michael Cooper <cooper@w3.org>
2022-02-08 11:50:38 -0500

    typo

---
+++ task-forces/personalization/minutes.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en" dir="ltr">
+	<head>
+		<?php include "../../_head.phi"; ?>
+		<title>Personalization Task Force Minutes</title>
+	</head>
+	<body>
+		<?php include "../../_header.phi"; ?>
+		<h1>Personalization Task Force Minutes</h1>
+		<?php
+	$channel_id = "personalization";
+	include "../../../../2022/01/minutes/default-view.phi";
+	?>
+		<?php include "../../_footer.phi"; ?>
+	</body>
+</html>
